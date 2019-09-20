7886203ccddf0846a2719e082243c45a359ca9d8
Will Howes <williameldenhowes@gmail.com>
2019-09-20 10:15:40 +0100

    Refactored to make the increment_integer method more readable

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -1,6 +1,9 @@
 def increment_string(string_input)
   just_letters = string_input.delete('0-9')
   just_numbers = string_input.delete('a-z')
-  return "#{just_letters}1" if just_numbers == ""
-  return result = "#{just_letters}#{just_numbers.next}"
+  if just_numbers == ""
+    return "#{just_letters}1"
+  else
+    return result = "#{just_letters}#{just_numbers.next}"
+  end
 end
