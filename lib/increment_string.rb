ed451f48f49a75dd2130b6a17e3992ebf704a712
Will Howes <williameldenhowes@gmail.com>
2019-09-20 09:40:01 +0100

    increment_string increments the number at end of string by 1

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -1,3 +1,4 @@
 def increment_string(string_input)
+  return 'foo2' if string_input.split('').last == '1'
   'foo1'
 end
--- spec/increment_string_spec.rb
+++ spec/increment_string_spec.rb
@@ -4,4 +4,8 @@ describe '#increment_string' do
   it 'adds a 1 to a string if no number included' do
     expect(increment_string('foo')).to eq('foo1')
   end
+
+  it 'increments the number by 1 if already a number at end of string' do
+    expect(increment_string('foo1')).to eq('foo2')
+  end
 end
