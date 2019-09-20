b6930d45d92d030fa0d73f7c0566cee89b3254ce
Will Howes <williameldenhowes@gmail.com>
2019-09-20 09:59:03 +0100

    Refactored to remove previous code as now covered by new code

--- lib/increment_string.rb
+++ lib/increment_string.rb
@@ -5,11 +5,8 @@ class String
 end
 
 def increment_string(string_input)
-  return 'foo2' if string_input.split('').last == '1'
-  return 'foo10' if string_input.split('').last == '9'
   just_letters = string_input.delete('^a-z')
   just_numbers = string_input.delete('^0-9')
   just_numbers = just_numbers.to_i + 1
   return result = "#{just_letters}#{just_numbers}"
-  'foo1'
 end
